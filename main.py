a69e33afcb847847a1f2a53662ea22524de51601
Ori <disori777@gmail.com>
2024-11-06 20:08:03 +0800

    Separate invoice number and date into two variables

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     invoice_df = pd.read_excel(filepath, sheet_name='Sheet 1')
+    invoice_nr = filepath[9:14]
+    invoice_date = filepath[15:24]
