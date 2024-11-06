6fba359c8d188b859ee97be35d9df02ded138d61
Ori <disori777@gmail.com>
2024-11-06 20:58:50 +0800

    Remove .split() from invoice_name and put .split() into invoice_nr and invoice_date so invoice_name could be used in output as name of the pdf.

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     invoice_df = pd.read_excel(filepath, sheet_name='Sheet 1')
-    invoice_name = Path(filepath).stem.split("-")
-    invoice_nr = f'Invoice nr. {invoice_name[0]}'
-    invoice_date = f'Date {invoice_name[1]}'
+    invoice_name = Path(filepath).stem
+    invoice_nr = f'Invoice nr. {invoice_name.split("-")[0]}'
+    invoice_date = f'Date {invoice_name.split("-")[1]}'
     invoice_pdf = FPDF(orientation='P', unit='mm', format='A4')
     invoice_pdf.set_auto_page_break(auto=False, margin=0)
 
@@ -18,4 +18,4 @@ for filepath in filepaths:
     invoice_pdf.set_font(family="Times", style="B", size=24)
     invoice_pdf.cell(w=0, h=12, txt=invoice_nr, align='L', ln=1)
     invoice_pdf.cell(w=0, h=12, txt=invoice_date, align='L', ln=1)
-    #invoice_pdf.output(f'{filepath[9:].replace(".xlsx", ".pdf")}')
+    invoice_pdf.output(f'PDFs/{invoice_name}.pdf')
